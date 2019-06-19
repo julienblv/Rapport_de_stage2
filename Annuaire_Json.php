b7bb0df14cac4b448b237ff37d9783543b68c1a3
JulienBlvt <julien.blavette31@gmail.com>
2019-06-19 10:03:42 +0200

    Fichierde base sructure php vide

---
+++ Annuaire_Json.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+<body>
+
+<h1> CPAM PCA </h1>
+
+<label>Vôtre CPAM :</label>
+    <option value="">--Choisissez une option--</option>
+    <option> Haute-Garonne</option>
+    <option> Tarn </option>
+    <option> Aveiron </option> 
+
+<?php
+
+
+
+?>
+
+
+
+</body>
+
+
+</head>
+
+
+</html>
