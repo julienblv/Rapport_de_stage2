7bc18a4747535ad58aba0152f331685f38e5a1de
JulienBlvt <julien.blavette31@gmail.com>
2019-06-19 12:28:57 +0200

    création d'un renvoi vers la page des modifications dans le tableau

---
+++ Modification_Annuaire.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+<body>
+
+<h1> CPAM PCA </h1>
+<h3>Modification_Annuaire</h3>
+        
+    </tr>
+</table>
+
+
+<?php
+
+
+
+?>
+
+
+
+</body>
+
+
+</head>
+
+
+</html>
