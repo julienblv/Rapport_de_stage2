4d4f0cd8151877e6b72aeabaf0d5c79ab47b6dd4
JulienBlvt <julien.blavette31@gmail.com>
2019-06-24 15:38:13 +0200

    obsolite_login_added

---
+++ Login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+<li><a href="http://localhost/Dossier%20Php/Cpam_CPA/Login.php"> Deconnexion ?</a></li>
+<head>
+
+<body>
+
+<h1> Bienvenue sur l'application CPAM PCA </h1>
+<h3>Veuillez vous connecter avec les identifiants qui vous on été distribués au préalable</h3>
+
+
+<div>
+Login:
+<input type = 'txtbox'>
+</div>
+
+<div>
+Password:
+<input type = 'txtbox'>
+</div>
+
+
+<form>
+
+<li><a href="http://localhost/Dossier%20Php/Cpam_CPA/Annuaire_Json.php"> Connexion </a></li>
+<br/>
+<br>
+</form>
+
+
+    </tr>
+</table>
+
+
+<?php
+
+
+
+?>
+
+
+</body>
+
+
+</head>
+
+
+</html>
