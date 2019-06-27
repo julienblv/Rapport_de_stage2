e9b9959116afeb15fc8e8672e420123ad3b02c9e
JulienBlvt <julien.blavette31@gmail.com>
2019-06-27 11:06:20 +0200

    last changes before testing PDO

--- Modif_Pt3.php
+++ Modif_Pt3.php
@@ -30,25 +30,27 @@
      
      //bien mais obsolete pour php5 a remplaçer avec msqli :
 
+     $server="http://localhost/127.0.0.1";
+     $user="root";
+     $password=" ";
+     $database="annuaire_comite_alerte";
 
-     mysqli_connect("127.0.0.1", "root", " ", "annuaire_comite_alerte");
-     if (!$link) {
-          die('Erreur de connexion : ' . mysqli_connect_error());
-      }
-      
-
-     mysql_select("annuaire") or die(mysql_error());
-     $UID=(int)$_GET['ID'];
-     $querry=mysql_query("SELECT * FROM annuaire_comite_alerte WHERE id='$UID'")or die(mysql_error());
+     mysqli_connect($server, $user, $password, $database);
      
-     if(mysql_num_rows($query)>=1){
-          while($row = mysql_fetch_array($query>=1)) {
+
+     $UID = (int)$_GET['ID'];
+     $query = mysqli_query("SELECT * FROM stokesley_students WHERE id = '$UID'") or die(mysql_error());
+
+     
+     if(mysqli_num_rows($query)>=1){
+          while($row = mysqli_fetch_row($query>=1)) {
                $nom = $row['nom'];
                $batiment=$row['batiment'];
                $etage=$row['etage'];
                $portable=$row['portable'];
                $fixe=$row['fixe'];
      }
+
 ?>
 <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="POST">
 
@@ -81,23 +83,23 @@ Fixe:<input type="text" name="ud_fixe" value="<?$nom;?>"><br>
 
 $ud_ID = (int)$_POST["ID"];
 
-$ud_nom = mysql_real_escape_string($_POST["ud_nom"]);
-$ud_batiment = mysql_real_escape_string($_POST["ud_batiment"]);
-$ud_etage = mysql_real_escape_string($_POST["ud_etage"]);
-$ud_portable= mysql_real_escape_string($_POST["ud_portable"]);
-$ud_fixe= mysql_real_escape_string($_POST["ud_fixe"]);
+$ud_nom = mysqli_real_escape_string($_POST["ud_nom"]);
+$ud_batiment = mysqli_real_escape_string($_POST["ud_batiment"]);
+$ud_etage = mysqli_real_escape_string($_POST["ud_etage"]);
+$ud_portable= mysqli_real_escape_string($_POST["ud_portable"]);
+$ud_fixe= mysqli_real_escape_string($_POST["ud_fixe"]);
 
 
-$query="UPDATE stokesley_students
+$query="UPDATE annuaire_comite_alerte 
         SET nom = '$ud_nom', batiment = '$ud_batiment', etage = '$ud_etage', portable='$ud_portable', fixe = '$ud_fixe' 
         WHERE ID='$ud_ID'";
 
 
-mysql_query($query)or die(mysql_error());
-if(mysql_affected_rows()>=1){
-echo "<p>($ud_ID) Record Updated<p>";
+mysqli_query($query)or die(mysqli_error());
+if(mysqli_affected_rows()>=1){
+echo "<p>($ud_ID) Mis à jour dans la base <p>";
 }else{
-echo "<p>($ud_ID) Not Updated<p>";
+echo "<p>($ud_ID) N'as été pris en compte dans la mise à jour de la base <p>";
 }
    
 
