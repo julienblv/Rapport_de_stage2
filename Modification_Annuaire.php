637b26b59b2126489741c4eb3cd21ab30670e03d
JulienBlvt <julien.blavette31@gmail.com>
2019-07-09 11:36:51 +0200

    Contact list finished

--- Modification_Annuaire.php
+++ Modification_Annuaire.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();
+    session_start();
 ?>
 
 <!DOCTYPE html>
@@ -7,41 +7,40 @@ session_start();
 <meta http-equiv="Content-type" content="text/html"/>
 <!-- mettre une condition si session null ou pas -->
 
+
+
 <head>
-<link rel ="stylesheet" href="http://localhost/Dossier%20Php/Cpam_CPA/bootstrap.min.css">
+    <link rel ="stylesheet" href="http://localhost/Dossier%20Php/Cpam_CPA/bootstrap.min.css">
 <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
-<link rel="stylesheet"  href="Style.css" type="text/css">
+    <link rel="stylesheet"  href="Style.css" type="text/css">
 </head>
+
+
 
 <body>
 
-
-
-<div class="container-fluid" id="color">
-    <h1 id="h1"> CPAM PCA </h1>
-
-
-    <div id = "deco">
-            <li><a href="http://localhost/Dossier%20Php/Cpam_CPA/Login.php"> Deconnexion ?</a></li>
-
-            </div>
-
-
+<div class="container-fluid">
+    <div class="txttitre">
+        <h1 id="h1"><div class="littlespace">CPAM PCA</div></h1>
         <h3 id="h1">Modification_Annuaire</h3>
+    </div>
 
-        
-        <br/>
-        
 
-            
+    <div class='liens'>    
+        <a  class="lien-color" href="http://localhost/Dossier%20Php/Cpam_CPA/Numeros_importants.json" download>Téléchargez le Json ici</a>
+            <div id = "deco">
+                <a  class="lien-color" href="http://localhost/Dossier%20Php/Cpam_CPA/Login.php"> Deconnexion ?</a>
+            </div>
+        <a class="lien-color" href="http://localhost/Dossier%20Php/Cpam_CPA/convertcsv.CSV" download>Téléchargez le CSV ici</a>
+            <br>
+            <br>
+    </div>
+
+
 </div>
 
+        
 
-<a href="http://localhost/Dossier%20Php/Cpam_CPA/Numeros_importants.json" download>Téléchargez le Json ici</a>
-
-<br/>
-
-<a href="http://localhost/Dossier%20Php/Cpam_CPA/convertcsv.CSV" download>Téléchargez le CSV ici</a>
 
 <?php
 
@@ -59,25 +58,20 @@ session_start();
 <?php
 
     echo "
-    <div class='container'>
     <table border='1'>
-    <tr>
-    <th>Id<th>
-    <th>Nom</th>
-    <th>Batiment</th>
-    <th>Etage</th>
-    <th>Portable</th>
-    <th>Fixe</th>
-    <th>Fonction</th>
-    <th>Niveau</th>
-    <th>Type</th>
-    <th>modification</th>
-    </tr>
-    </br>
-    </br>
-    
-
-    " ;
+    <tr class='container-fluid'>
+    <th><div class='colotxttab'>Id</div></th>
+    <th><div class='colotxttab'>Nom</div></th>
+    <th><div class='colotxttab'>Batiment</div></th>
+    <th><div class='colotxttab'>Etage</div></th>
+    <th><div class='colotxttab'>Portable</div></th>
+    <th><div class='colotxttab'>Fixe</div></th>
+    <th><div class='colotxttab'>Fonction</div></th>
+    <th><div class='colotxttab'>Niveau</div></th>
+    <th><div class='colotxttab'>Type</div></th>
+    <th><div class='colotxttab'>Modification</div></th>
+    <th><div class='colotxttab'>Supression</div></th>
+    </tr>" ;
 
     //initialisation de resultat dans la boucle + Affichage de la requête avec fetch()//
     while($resultat = $req->fetch()){
@@ -98,31 +92,34 @@ session_start();
 ?>
     </form>
 
+    <div class="container-fluid">
+    </div>
+
 <?php
 
 echo
     
-"<td>".$resultat['nom']."</td></a>
-<td>".$resultat['batiment']."</td></a>
-<td>".$resultat['etage']."</td></a>
-<td>".$resultat['portable']."</td></a>
-<td>".$resultat['fixe']."</td></a>
-<td>".$resultat['fonction']."</td></a>
-<td>".$resultat['niveau']."</td></a>
-<td>".$resultat['type']."</td></a>
-<td></td></a>
-<td></td></a>";
+"<td class='container'>".$resultat['nom']."</td></a>
+<td class='container'>".$resultat['batiment']."</td></a>
+<td class='container'>".$resultat['etage']."</td></a>
+<td class='container'>".$resultat['portable']."</td></a>
+<td class='container'>".$resultat['fixe']."</td></a>
+<td class='container'>".$resultat['fonction']."</td></a>
+<td class='container'>".$resultat['niveau']."</td></a>
+<td class='container'>".$resultat['type']."</td></a>
+
+";
 
 
 if ($_SESSION['admin']=="yes"){
     echo 
-    "<td><form method='POST' action='Modif_Pt3.php?id='".$_POST['id']."'>
-    <input type='submit' class='btn btn-default btn-lg' value='Modifier ?".$_POST['id']." ?'/></td></a>
+    "<td class='container'><form method='POST' action='Modif_Pt3.php?id='".$_POST['id']."'>
+    <input type='submit' class='btn btn-default btn-lg' value='Modifier ? (".$_POST['id']." )'/></td></a>
     </form>
 
     <td>
     <form method='POST' action='Delete.php?id='".$_POST['id']."'>
-    <input type='submit' class='btn btn-default btn-lg' value='Supprimer ?".$_POST['id']." ?'/></td></a>
+    <input type='submit' class='btn btn-default btn-lg' value='Supprimer ? (".$_POST['id']." )'/></td></a>
     </form>";
 }
 
@@ -134,14 +131,10 @@ if ($_SESSION['admin']=="yes"){
     }
 
     echo "</table>
-    ";
+    <//div>";
 
 //partie envoi du contact
 
-
-var_dump($_POST['nom']);
-print_r($_POST['nom']);
-var_dump($resultat['id']);
 ?>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
